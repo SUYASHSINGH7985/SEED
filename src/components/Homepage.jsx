47e7da7ffa6ca881cbda5fb8641a68753cbbb370
Suyash <singhsuyash012@gmail.com>
2025-07-27 10:59:35 +0530

    temporary changes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeroSection from './HeroSection';
+import HeroSection from './Herosection';
 import SecondSection from './SecondSection';
 import AISection from './AISection';
 import InvestmentSection from './InvestmentSection';
